423a61b917914c3393970d49db848068876b8dab
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-28 11:46:32 +0300

    add admin views

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,7 @@
 @section('title', 'Posts')
 @section('content')
     <h1>Posts</h1>
-    <a href="{{$posts->previousPageUrl()}}">Prev</a>
-    <a href="{{$posts->nextPageUrl()}}">Next</a>
+    {{$posts->links()}}
     <div class="row row-cols-4">
         @foreach($posts as $post)
             <div class="col">
@@ -12,10 +11,21 @@
                     <div class="card-body">
                         <h5 class="card-title">{{$post->title}}</h5>
                         <p class="card-text">{{ $post->snippet }}</p>
+                        <p class="text-muted" data-bs-toggle="tooltip" title="{{$post->created_at}}">{{ $post->created_at->diffForHumans() }}</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
         @endforeach
     </div>
+
 @endsection
+
+@push('js')
+    <script>
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+    </script>
+@endpush
